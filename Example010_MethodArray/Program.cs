139a1f3e858904edf991cb19e8155c8bfb0f1e1c
Belkin Dmitry <dmitry485@gmail.com>
2022-10-31 23:11:31 +0500

    git Example 011 ArrayLibrary

--- Example010_MethodArray/Program.cs
+++ Example010_MethodArray/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿int[] array = { 11, 21, 13, 14, 51, 16, 51, 71, 18 };
+
+int n = array.Length;
+int find = 51;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    // index = index + 1;
+    index++;
+}
