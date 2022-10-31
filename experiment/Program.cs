aa5c3e3e617b796fe2be3aa187636160e602ac1d
Belkin Dmitry <dmitry485@gmail.com>
2022-10-31 21:17:10 +0500

    add Example 008 IntroMethod

--- experiment/Program.cs
+++ experiment/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Console.Clear();
+
+// int i = 0;
+
+// while (i < 10)
+// {
+//     i += 2;
+//     Console.WriteLine(i);
+// }
+
+// // Семинар 1
+
+// // 1 Напишите программу, которая на вход принимает число и выдает его квадрат
+Console.Clear();
+
+Console.Write("Введите число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+// int sqrt = number * number;
+// number *= number;
+// number = number *number; 
+
+int sqrt1 = Convert.ToInt32(Math.Pow(number, 2));
+
+Console.WriteLine("Число в квадрате: " + sqrt1);
