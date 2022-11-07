128f25a6e38b14547475af1e5f2ea1c043d2dc7c
Belkin Dmitry <dmitry485@gmail.com>
2022-11-08 00:19:11 +0500

    add text and examples

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -1,2 +1,149 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+// // Вид 1   Метод  не возвращает/ не принимает.
+
+// void Method1()
+// {
+//     Console.WriteLine("Автор ....");
+// }
+// // Method1();
+
+
+
+// // Вид 2   Метод  не возвращает/ принимает.
+
+// void Method2(string msg) //msg = message
+// {
+//     Console.WriteLine(msg);
+// }
+// Method2(msg); //Method2("Текст сообщения"); Method2(msg: "Текст сообщения")
+
+// void Method21(string msg, int count)
+// {
+//     int i = 0;
+//     while (i < count)
+//     {
+//         Console.WriteLine(msg);
+//         i++;
+//     }
+// }
+// Method21("Текст", 4); //Method21(msg: "Текст", count: 4);
+
+
+// // Вид 3   Метод   возвращает/ не принимает.
+
+// int Method3()
+// {
+//     return DateTime.Now.Year;
+// }
+
+// int year = Method3();
+// Console.WriteLine(year);
+
+// // Вид 4   Метод   возвращает/ принимает.
+
+// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = String.Empty;  // ""; // изначально пустая строка
+
+//     while (i < count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+// }
+
+
+// string Method4(int count, string text)
+// {
+
+//     string result = String.Empty;  // ""; // изначально пустая строка
+//     for (int i = 0; i < count; i++)
+//     {
+//         result = result + text;
+//     }
+//     return result;
+// }
+
+// string res = Method4(10, "qwerty");
+// Console.WriteLine(res);
+
+
+// Цикл for
+
+// for (int i = 2; i <= 10; i++)
+// {
+//     for (int j = 2; j <= 10; j++)
+//     {
+//         Console.WriteLine($"{i}*{j}={i*j}");
+//     }
+//     Console.WriteLine();
+// }
+
+//====== Работа с текстом.
+// Дан текст. В тексте нужно все пробелы заменить черточками,
+// маленькие буквы "к" заменить большими "К",
+// а большие "С" заменить маленькими "с".
+
+// string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
+//               + "ежели бы вас послали вместо нашего милого Винценгероде,"
+//               + "вы бы взяли приступом согласие прусского короля. "
+//               + "Вы так красноречивы. Вы дадите мне чаю?";
+// // string s = "qwerty"
+// //             012
+// // s[3] // r
+
+// string Replace(string text, char oldValue, char newValue)
+// {
+//     string result = String.Empty;
+
+//     int Length = text.Length;
+//     for (int i = 0; i < Length; i++)
+//     {
+//         if (text[i] == oldValue) result = result + $"{newValue}";
+//         else result = result + $"{text[i]}";
+//     }
+
+//     return result;
+// }
+
+// string newText = Replace(text, ' ', '|');
+// Console.WriteLine(newText);
+// Console.WriteLine();
+// string newText = Replace(newText, 'к', 'К');
+
+
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1, };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
+
+PrintArray(arr);
