3507a55070b69a2df27494a4a017a021afe924f0
Belkin Dmitry <dmitry485@gmail.com>
2022-11-10 00:03:02 +0500

    add 4 examples

--- Experiment_Seminar/Program.cs
+++ Experiment_Seminar/Program.cs
@@ -146,3 +146,136 @@
 // {
 //     Console.WriteLine("первое число не является квадратом другого");
 // }
+
+
+//  SEMINAR3 _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+
+// Задача 17. Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+// int GetQuarterFromCoordinate(int userX, int userY)
+// {
+//     int quarter = 0;
+//     if (userX > 0 && userY > 0)
+//     {
+//         quarter = 1;
+//     }
+//     else if (userX < 0 && userY > 0)
+//     {
+//         quarter = 2;
+//     }
+//     else if (userX < 0 && userY < 0)
+//     {
+//         quarter = 3;
+//     }
+//     else if (userX > 0 && userY < 0)
+//     {
+//         quarter = 4;
+//     }
+//     return quarter;
+// }
+// Console.WriteLine("Введите координату X");
+// int userX = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату Y");
+// int userY = Convert.ToInt32(Console.ReadLine());
+
+// int quarter = GetQuarterFromCoordinate(userX, userY);
+// if (quarter != 0)
+// {
+//     Console.WriteLine($"Точка с координатами [{userX}:{userY}] находится в {quarter} четверти");
+// }
+// else
+// {
+//     Console.WriteLine($"Точка с координатами [{userX}:{userY}] находится на одной из осей");
+// }
+
+
+
+
+
+// Задача 18: Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+// void GetCoordinateFromQuarter(int Quarter)
+// {
+//     if (Quarter == 1)
+//     {
+//         Console.WriteLine("x>0 and y>0");
+//     }
+//     else if (Quarter == 2)
+//     {
+//         Console.WriteLine("x<0 and y>0");
+//     }
+//     else if (Quarter == 3)
+//     {
+//         Console.WriteLine("x<0 and y<0");
+//     }
+//     else if (Quarter == 4)
+//     {
+//         Console.WriteLine("x>0 and y<0");
+//     }
+//     else
+//     {
+//         Console.WriteLine("uncorrect quarter");
+//     }
+
+// }
+// Console.WriteLine("Введите номер четверти");
+// int Quarter = Convert.ToInt32(Console.ReadLine());
+// GetCoordinateFromQuarter(Quarter);
+
+
+
+
+
+// Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+// A (3,6); B (2,1) -> 5,09
+// A (7,-5); B (1,-1) -> 7,21
+// d = √(xb - xa)2 + (yb - ya)2
+
+// Console.Clear();
+// double Distance(int x1, int y1, int x2, int y2)
+// {
+//     double d = 0;
+//     d = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+//     return d;
+// }
+// Console.WriteLine("enter coordinate");
+// int x1 = Convert.ToInt32(Console.ReadLine());
+// int y1 = Convert.ToInt32(Console.ReadLine());
+// int x2 = Convert.ToInt32(Console.ReadLine());
+// int y2 = Convert.ToInt32(Console.ReadLine());
+
+// double d = Distance(x1, y1, x2, y2);
+// Console.WriteLine(Math.Round(d, 2));
+
+//для 3D
+// {Math.Pow((Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2) * 1.0), 0.5)}"  
+
+
+
+
+// Задача 22: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+// void sqrt(int N)
+// {
+//     if (N > 0)
+//     {
+//         int count = 1;
+//         while (count < N)
+//         {
+//             int result = count * count;
+//             Console.Write(result + ", ");
+//             count++;
+//         }
+//         Console.Write(N * N);
+//     }
+//     else
+//     {
+//         Console.WriteLine("Введите число больше 1");
+//     }
+// }
+// Console.WriteLine("enter number: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// sqrt(N);
+
